cd65f17a293e7b17eefcb336f3500499e0a54b5b
monke7 <mishraadwait456@gmail.com>
2026-01-08 01:33:45 +0530

    feat: setup E2B V2 sandbox infrastructure and build scripts

--- inngest/functions.ts
+++ inngest/functions.ts
@@ -1,9 +1,19 @@
 import { inngest } from "./client";
 import { createAgent, gemini } from "@inngest/agent-kit"
-export const helloWorld = inngest.createFunction(
+import Sandbox from "e2b"
+
+
+export const Jarvis = inngest.createFunction(
   { id: "Jarvis" },
   { event: "agent/Jarvis" },
   async ({ event, step }) => {
+
+    const sandboxId = await step.run("get-sandbox-id", async() => {
+      const sandbox = await Sandbox.create("letHimCode-nextjs-dev")
+      return sandbox.sandboxId;
+    })
+    
+
   const agent = createAgent({
     name: "Jarvis",
     description: "A simple AI assistant",
@@ -12,6 +22,13 @@ export const helloWorld = inngest.createFunction(
   })
 
   const { output } = await agent.run("Tell me a joke about programmers.")
+
+  const sandboxUrl = await step.run("get-sandbox-url", async() => {
+    const sandbox = await Sandbox.connect(sandboxId);
+    const host = sandbox.getHost(3000);
+
+    return `http://${host}`;
+  })
 
   return {
     message : (output[0] as any).content,
